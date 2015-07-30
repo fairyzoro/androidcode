e25a43c67a0034240b390cc1b7979932332e6318
fairyzoro <fairyzoro@gmail.com>
2015-07-30 21:09:09 +0800

    a

--- MyApplication2/app/src/main/java/com/sandroid/myapplication2/app/MainActivity.java
+++ MyApplication2/app/src/main/java/com/sandroid/myapplication2/app/MainActivity.java
@@ -1,7 +1,7 @@
 package com.sandroid.myapplication2.app;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,7 +12,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
 

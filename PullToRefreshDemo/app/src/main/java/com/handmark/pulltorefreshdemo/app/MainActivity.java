0b181422f580f75cb42cbd83407c9f30b9f36172
fairyzoro <fairyzoro@gmail.com>
2015-08-06 21:15:05 +0800

    PullToRefresh 基础Demo完成

--- PullToRefreshDemo/app/src/main/java/com/handmark/pulltorefreshdemo/app/MainActivity.java
+++ PullToRefreshDemo/app/src/main/java/com/handmark/pulltorefreshdemo/app/MainActivity.java
@@ -1,39 +1,92 @@
 package com.handmark.pulltorefreshdemo.app;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements PullToRefreshBase.OnRefreshListener2{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // 获取PullToRefreshListView
+        PullToRefreshListView pullToRefreshListView =
+                (PullToRefreshListView) findViewById(R.id.id_pulltorefresh);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        // 1、设置PullToRefreshListView支持的方向，上拉、下拉、还是上下都支持
+        pullToRefreshListView.setMode(PullToRefreshBase.Mode.BOTH);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // 2、设置数据Adapter
+        ArrayList<String> strings = new ArrayList<String>();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        for (int i = 0; i < 20; i++) {
+            strings.add("数字:" + i);
         }
 
-        return super.onOptionsItemSelected(item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, strings
+        );
+
+        pullToRefreshListView.setAdapter(adapter);
+
+        // 3、设置PullToRefresh 的刷新接口
+        pullToRefreshListView.setOnRefreshListener(this);
+        // 自己手写上面的implements
+
+        // 4. 获取下拉刷新内部的内容控件
+        // 因为 PullToRefreshListView 内部包裹ListView，那么返回的也是ListView
+//        ListView listView = pullToRefresh.getRefreshableView();
+
+//        listView.setAdapter(adapter);
+        // TODO 这里并没有对listView做进一步尝试,留待以后了
+    }
+
+    @Override
+    public void onPullDownToRefresh(final PullToRefreshBase refreshView) {
+        Toast.makeText(this, "下拉", Toast.LENGTH_SHORT).show();
+
+        AsyncTask<String, Integer, String> task =
+                new AsyncTask<String, Integer, String>() {
+
+                    @Override
+                    protected String doInBackground(String... params) {
+
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        return "";
+                    }
+
+                    @Override
+                    protected void onPostExecute(String s) {
+
+                        // 关闭加载动画,清除状态
+                        // 让另一个方向的刷新动画可以显示
+                        refreshView.onRefreshComplete();
+                        //super.onPostExecute(s);
+                    }
+                }.execute();
+
+    }
+
+    @Override
+    public void onPullUpToRefresh(PullToRefreshBase refreshView) {
+        Toast.makeText(this,"上拉",Toast.LENGTH_SHORT).show();
     }
 }
